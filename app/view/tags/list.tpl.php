9bb2be7087ba15262560003d84f43ce8dc14a881
Anton Petersson <anton@anp.nu>
2016-06-03 21:40:35 +0200

    Tag list now links to questions/tagged/<tag-name>

--- app/view/tags/list.tpl.php
+++ app/view/tags/list.tpl.php
@@ -3,13 +3,11 @@
 <ul>
 <?php foreach ($tags as $tag) : ?>
 
-<li><?=$tag->name?></li>
+<li><a href='<?=$this->url->create("questions/tagged/$tag->name")?>'><?=$tag->name?></a></li>
 <ul>
 <li><?=$tag->description?></li>
 </ul>
 
 <?php endforeach; ?>
 </ul>
-
-<p><a href='<?=$this->url->create('')?>'>Home</a></p>
 
