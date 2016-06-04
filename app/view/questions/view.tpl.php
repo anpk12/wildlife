1225c90839f7d97a178a3d222b817dc716e99876
Anton Petersson <anton@anp.nu>
2016-06-05 00:08:14 +0200

    Change presentation of questions and answers

--- app/view/questions/view.tpl.php
+++ app/view/questions/view.tpl.php
@@ -2,50 +2,68 @@
 
 <table class="question_view">
 <tr>
-<td><?=$question->topic?></td>
-<td><?=$question->userAcronym?></td>
-<td><?=$question->created?></td>
+<th colspan="2"><?=$question->topic?></td>
 </tr>
 <tr>
+<td>0 votes</td>
 <td><?=$this->textFilter->doFilter($question->text, 'shortcode, markdown')?></td>
 </tr>
 <tr>
-<td>
+<td colspan="2">
 <?php foreach ($question->tags as $tag) : ?>
-<?=$tag->name?>, 
+<a href="<?=$this->url->create("questions/tagged/$tag->name")?>"><?=$tag->name?></a> 
 <?php endforeach; ?>
 </td>
 </tr>
+<tr>
+<td>by <a href="<?=$this->url->create('users/id/'.$question->userid)?>"><?=$question->userAcronym?></a></td>
+<td><?=$question->created?></td>
+</tr>
+<tr>
+<td colspan="2">
+<ul>
 <?php foreach ($question->comments as $qc) : ?>
-<tr>
-<td>
-    <p><?=$this->textFilter->doFilter($qc->content, 'shortcode, markdown')?> by <?=$qc->user->acronym?></p>
+<li>
+    <p><?=$this->textFilter->doFilter($qc->content, 'shortcode, markdown')?> by <a href="<?=$this->url->create('users/id/'.$qc->user->id)?>"><?=$qc->user->acronym?></a></p>
+</li>
+<?php endforeach; ?>
+</ul>
 </td>
 </tr>
-<?php endforeach; ?>
 <tr>
-<td>
+<td colspan="2">
     <p><a href='<?=$this->url->create('comment2/comment-question/' . $question->id)?>'>Comment</a></p>
 </td>
 </tr>
-
+</table>
+<table class="answers_view">
+<tr>
+<th colspan="2">Answers</th>
+</tr>
 <?php foreach ($answers as $a) : ?>
-
 <tr>
 <td><?=$a->votes?> votes</td>
 <td><?=$this->textFilter->doFilter($a->text, 'shortcode, markdown')?></td>
-<td><?=$a->userAcronym?></td>
+</tr>
+<tr>
+<td><a href="<?=$this->url->create('users/id/'.$a->userid)?>"><?=$a->userAcronym?></a></td>
 <td><?=$a->created?></td>
 </tr>
-<?php foreach ($a->comments as $ac) : ?>
 <tr>
-<td>
-    <p><?=$this->textFilter->doFilter($ac->content, 'shortcode, markdown')?> by <?=$ac->user->acronym?></p>
+<td colspan="2">
+<ul>
+<?php foreach ($a->comments as $ac) : ?>
+
+<li>
+    <p><?=$this->textFilter->doFilter($qc->content, 'shortcode, markdown')?> by <a href="<?=$this->url->create('users/id/'.$qc->user->id)?>"><?=$qc->user->acronym?></a></p>
+</li>
+
+<?php endforeach; ?>
+</ul>
 </td>
 </tr>
-<?php endforeach; ?>
 <tr>
-<td>
+<td colspan="2">
 <p><a href='<?=$this->url->create('comment2/comment-answer/'
     . $question->id . '/' .$a->id)?>'>Comment
 </a></p>
